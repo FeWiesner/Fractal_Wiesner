63ebeb53a1b72a02bb774e72b8b36d77e8e17481
Fede Wiesner <f.wiesner@uniandes.edu.co>
2022-09-05 13:27:52 -0500

    added new criteria and adapted code to read them

--- 3d_fractal.py
+++ 3d_fractal.py
@@ -4,149 +4,166 @@ from matplotlib import pyplot as plt
 from mpl_toolkits import mplot3d
 
 #cambiar archivo
-network = ['HANO_','BALE_']
-side =['d', 'i', 'k']
+network = ['HANO_','BALE_', 'FOSO_', 'MODE_']
+side =['derecho_', 'izquierdo_', 'knee_']
+criterion = ['HGL', 'd', 'Q']
 
 for net in network:
     for s in side:
-                
-        X = []
-        Y = []
-        Z = []
+        for crit in criterion:
+                    
+            X = []
+            Y = []
+            Z = []
 
-        with open(net+s+'.txt') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            line_count = 0
-            for row in csv_reader:
-                if net == 'HANO_':
-                    a = 100
-                    b = -4
-                    fine = 16j
-                else: 
-                    a = 1
-                    b = -5.5
-                    fine = 90j
-                X.append(float(row[0])/a)
-                Y.append(float(row[1])/a)
-                Z.append(float(row[2]))
+            with open(net+s+crit+'.txt') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                line_count = 0
+                for row in csv_reader:
+                    if crit == 'HGL':
+                        b = 1
+                    elif crit == 'd':
+                        b = 100
+                    elif crit == 'Q':
+                        b = 100
+                    if net == 'HANO_' or net =='FOSO_':
+                        a = 100
+                        fine = 16j
+                        X.append(float(row[0])/a)
+                        Y.append(float(row[1])/a)
+                        Z.append(float(row[2])*b)
+                    elif net == 'MODE_':
+                        a = 100
+                        fine = 70j
+                        X.append((float(row[0])-1640000)/a)
+                        Y.append((float(row[1])-4940000)/a)
+                        Z.append(float(row[2])*b)
+                    else: 
+                        a = 1
+                        fine = 90j
+                        X.append(float(row[0])/a)
+                        Y.append(float(row[1])/a)
+                        Z.append(float(row[2]))
 
-        xmax, ymax, zmax = max(X), max(Y), max(Z)
-        xmin, ymin, zmin = min(X), min(Y), min(Z)
+            xmax, ymax, zmax = max(X), max(Y), max(Z)
+            xmin, ymin, zmin = min(X), min(Y), min(Z)
 
-        maxi = (xmax, ymax, zmax)
-        mini = (xmin, ymin, zmin)
+            maxi = (xmax, ymax, zmax)
+            mini = (xmin, ymin, zmin)
 
-        from scipy.interpolate import griddata
+            from scipy.interpolate import griddata
 
-        points = []
-        for i in range(len(X)):
-            points.append([X[i], Y[i]])
-        points = np.array(points)
-        Z = np.array(Z)
-
-
-        grid_x, grid_y = np.mgrid[xmin:xmax:fine, ymin:ymax:fine]
-        grid = griddata(points, Z, (grid_x, grid_y), method='cubic')
-
-        grid = np.nan_to_num(grid, nan = -1)
-
-        vectors = []
-        r = int(str(fine)[:-1])
-        for i in range(r):
-            for j in range(r):
-                if grid[i][j] != -1:
-                    punto = [grid_x[i][0], grid_y[0][j], grid[i][j]]
-                    vectors.append(punto)
-
-        print(len(vectors))
-
-        '''
-        ax = plt.axes(projection = '3d')
-        for i in vectors:
-            ax.scatter3D(i[0], i[1], i[2], color = 'red')
-        plt.show()
+            points = []
+            for i in range(len(X)):
+                points.append([X[i], Y[i]])
+            points = np.array(points)
+            Z = np.array(Z)
 
 
-        plt.imshow(grid.T, extent=(xmin, xmax, ymin, ymax), origin='lower')
-        plt.title('Cubic')
-        plt.show()
-        '''
+            grid_x, grid_y = np.mgrid[xmin:xmax:fine, ymin:ymax:fine]
+            grid = griddata(points, Z, (grid_x, grid_y), method='cubic')
+
+            grid = np.nan_to_num(grid, nan = -1)
+
+            vectors = []
+            r = int(str(fine)[:-1])
+            for i in range(r):
+                for j in range(r):
+                    if grid[i][j] != -1:
+                        punto = [grid_x[i][0], grid_y[0][j], grid[i][j]]
+                        vectors.append(punto)
+
+            print(len(vectors))
+
+            '''
+            ax = plt.axes(projection = '3d')
+            for i in vectors:
+                ax.scatter3D(i[0], i[1], i[2], color = 'red')
+            plt.show()
 
 
-        def count_xrow(xinf, xsup, yinf, ysup, zinf, zsup, vec, eps):
-            cuenta = 0
-            while xsup <= mini[0]+length+10:
-                #for a single box
-                for v in vec:
-                    if v[0]>=xinf and v[0]<= xsup:
-                        if v[1]>=yinf and v[1]<= ysup:
-                            if v[2]>=zinf and v[2]<= zsup:
-                                cuenta += 1
-                                vec.remove(v)
-                                break
-                #to the next box on the same x-row
-                xinf = xsup
-                xsup += eps
-            return cuenta
+            plt.imshow(grid.T, extent=(xmin, xmax, ymin, ymax), origin='lower')
+            plt.title('Cubic')
+            plt.show()
+            '''
 
 
-        length = max(xmax-xmin, ymax-ymin, zmax-zmin)
-        boxsize = []
-        boxcount = []
+            def count_xrow(xinf, xsup, yinf, ysup, zinf, zsup, vec, eps):
+                cuenta = 0
+                while xsup <= mini[0]+length+10:
+                    #for a single box
+                    for v in vec:
+                        if v[0]>=xinf and v[0]<= xsup:
+                            if v[1]>=yinf and v[1]<= ysup:
+                                if v[2]>=zinf and v[2]<= zsup:
+                                    cuenta += 1
+                                    vec.remove(v)
+                                    break
+                    #to the next box on the same x-row
+                    xinf = xsup
+                    xsup += eps
+                return cuenta
 
-        def number(f,n):
-            eps = length/(f**n)
-            count = 0
-            vecs = vectors.copy()
-            xinf = mini[0]-1
-            yinf = mini[1]-1
-            zinf = mini[2]-1
 
-            xsup = xinf + eps
-            ysup = yinf + eps
-            zsup = zinf + eps
+            length = max(xmax-xmin, ymax-ymin, zmax-zmin)
+            boxsize = []
+            boxcount = []
 
-            while zsup <= mini[2]+length+10:
-                
-                while ysup <= mini[1]+length+10:
-                    count += count_xrow(xinf, xsup, yinf, ysup, zinf, zsup, vecs, eps)
-                    xinf = mini[0]-1
-                    xsup = xinf + eps
-                    yinf = ysup
-                    ysup = yinf + eps
+            def number(f,n):
+                eps = length/(f**n)
+                count = 0
+                vecs = vectors.copy()
+                xinf = mini[0]-1
                 yinf = mini[1]-1
+                zinf = mini[2]-1
+
+                xsup = xinf + eps
                 ysup = yinf + eps
-                zinf = zsup
                 zsup = zinf + eps
 
-            return(count,eps)
+                while zsup <= mini[2]+length+10:
+                    
+                    while ysup <= mini[1]+length+10:
+                        count += count_xrow(xinf, xsup, yinf, ysup, zinf, zsup, vecs, eps)
+                        xinf = mini[0]-1
+                        xsup = xinf + eps
+                        yinf = ysup
+                        ysup = yinf + eps
+                    yinf = mini[1]-1
+                    ysup = yinf + eps
+                    zinf = zsup
+                    zsup = zinf + eps
+
+                return(count,eps)
 
 
-        import scipy.stats
+            import scipy.stats
 
-        for i in range(1,100):
-            ans = number(i,1)
-            boxcount.append(ans[0])
-            boxsize.append(1/ans[1])
-            if i > r/2:
-                bc = np.array(boxcount)
-                bs = np.array(boxsize)
-                lg_bc = np.log(bc)
-                lg_bs = np.log(bs)
-                slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(lg_bs,lg_bc)
-                if r_value > 0.99:
-                    break
+            for i in range(1,100):
+                ans = number(i,1)
+                boxcount.append(ans[0])
+                boxsize.append(1/ans[1])
+                if i > r/2:
+                    bc = np.array(boxcount)
+                    bs = np.array(boxsize)
+                    lg_bc = np.log(bc)
+                    lg_bs = np.log(bs)
+                    slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(lg_bs,lg_bc)
+                    if r_value > 0.99:
+                        break
+            
+            '''
+            fig, ax = plt.subplots()
+            ax.scatter(lg_bs, lg_bc)
+            ax.set_xlabel('log(1/e)')
+            ax.set_ylabel('log(N(e)')
+            ax.set_title('Box Dimension Regression')
+            ax.text(b,1, 'log(N(e)) = '+str(round(intercept,2))+' + ' + str(round(slope,2)) + 'log(1/e)')
+            ax.plot([min(lg_bs),max(lg_bs)], [intercept + slope * min(lg_bs), intercept + slope * max(lg_bs)], color="k", lw=2.5)
+            plt.savefig(net+s+'.png')
+            '''
 
-        fig, ax = plt.subplots()
-        ax.scatter(lg_bs, lg_bc)
-        ax.set_xlabel('log(1/e)')
-        ax.set_ylabel('log(N(e)')
-        ax.set_title('Box Dimension Regression')
-        ax.text(b,1, 'log(N(e)) = '+str(round(intercept,2))+' + ' + str(round(slope,2)) + 'log(1/e)')
-        ax.plot([min(lg_bs),max(lg_bs)], [intercept + slope * min(lg_bs), intercept + slope * max(lg_bs)], color="k", lw=2.5)
-        plt.savefig(net+s+'.png')
-
-        with open('results.txt', 'a', encoding = 'utf-8') as f:
-            f.write(str(net+s)+'\n')
-            f.write(str(r_value)+'\n')
-            f.write('Dim Fractal = '+ str(slope)+ '\n\n')
+            with open('results.txt', 'a', encoding = 'utf-8') as f:
+                f.write(str(net+s)+'\n')
+                f.write(str(r_value)+'\n')
+                f.write('Dim Fractal = '+ str(slope)+ '\n\n')
